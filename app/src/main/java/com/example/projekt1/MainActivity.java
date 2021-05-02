e3440e27241cbfd74d8b997da6879c0c0499d000
IDxstyy <marco.heusohn@mnd.thm.de>
2021-05-02 14:32:31 +0200

    Version 0.0.0.1.1

--- app/src/main/java/com/example/projekt1/MainActivity.java
+++ app/src/main/java/com/example/projekt1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Lul der erste Code
+        //Lul der erste Code V2
     }
 }
