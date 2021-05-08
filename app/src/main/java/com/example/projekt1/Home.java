cdb3727220d088cc490f5d3466e7ed49f529eefb
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2021-05-08 14:25:09 +0200

    Recylcer View Implementation

---
+++ app/src/main/java/com/example/projekt1/Home.java
@@ -0,0 +1,64 @@
+package com.example.projekt1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Home extends RecyclerView.Adapter<Home.ViewHolder> {
+
+    String data[];
+    Context context;
+
+    public Home(){
+        this.data = this.getChats();
+    }
+
+    public Home(String[] data){
+
+    }
+
+    @NonNull
+    @Override
+    public Home.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.home_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Home.ViewHolder holder, final int position) {
+        holder.textView.setText(data[position]);
+        holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicked on "+ data[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return getChats().length;
+    }
+
+    public String[] getChats(){
+        return new String[] {"Chat1", "Chat2", "Chat3", "Chat4", "Chat5"};
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.home_textView_chat);
+        }
+    }
+}
--- app/src/main/java/com/example/projekt1/MainActivity.java
+++ app/src/main/java/com/example/projekt1/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.projekt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    RecyclerView recyclerView;
+    Home home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Lul der erste Code V37 Milliarden und 2
+        // Home - RecyclerView - Implementation
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        home = new Home();
+        recyclerView.setAdapter(home);
     }
 }
