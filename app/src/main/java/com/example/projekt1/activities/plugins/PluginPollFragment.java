600fc31403bcbae47ce47d0a698b279e8a356fdc
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2021-09-16 14:42:49 +0200

    added PluginBaseFragment with firebase connection

--- app/src/main/java/com/example/projekt1/activities/plugins/PluginPollFragment.java
+++ app/src/main/java/com/example/projekt1/activities/plugins/PluginPollFragment.java
@@ -2,18 +2,27 @@ package com.example.projekt1.activities.plugins;
 
 import com.example.projekt1.R;
 import com.example.projekt1.models.plugins.Plugin;
-import com.example.projekt1.models.plugins.pluginData.Notiz;
+import com.google.firebase.database.DataSnapshot;
 
 public class PluginPollFragment extends PluginBaseFragment {
     @Override
-    public Plugin initializePlugin() {
+    public void initializePlugin() {
         this.layout= R.layout.fragment_notizen_plugin;
+        ;
+    }
 
-        return new Plugin<Poll>() {
-            @Override
-            public void doPluginStuff() {
+    @Override
+    public Plugin setNewPlugin() {
+        return null;
+    }
 
-            }
-        };
+    @Override
+    protected Plugin castToSpecifiedPlugin(DataSnapshot pluginSpecificData, DataSnapshot pluginDataList) {
+        return null;
+    }
+
+    @Override
+    protected void setPluginLayout() {
+
     }
 }
