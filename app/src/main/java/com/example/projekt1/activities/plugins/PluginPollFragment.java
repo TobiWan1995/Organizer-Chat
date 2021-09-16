727aaa2a1233a3e9b46370a5175f99e9d433b11d
mrkhmr <trashme23@gmail.com>
2021-09-16 18:04:35 +0200

    Added classes: PluginPollFragment, Poll, PluginPoll Implemented PluginPoll to App

--- app/src/main/java/com/example/projekt1/activities/plugins/PluginPollFragment.java
+++ app/src/main/java/com/example/projekt1/activities/plugins/PluginPollFragment.java
@@ -2,27 +2,46 @@ package com.example.projekt1.activities.plugins;
 
 import com.example.projekt1.R;
 import com.example.projekt1.models.plugins.Plugin;
+import com.example.projekt1.models.plugins.PluginPoll;
+import com.example.projekt1.models.plugins.pluginData.Poll;
 import com.google.firebase.database.DataSnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PluginPollFragment extends PluginBaseFragment {
     @Override
     public void initializePlugin() {
-        this.layout= R.layout.fragment_notizen_plugin;
+
         ;
     }
 
     @Override
     public Plugin setNewPlugin(String key) {
-        return null;
+
+        return new PluginPoll(key, "Mit diesem Plugin lassen sich Abstimmungen durchführen", this.chatId, this.pluginType) {
+         @Override
+         public void doPluginStuff() {
+
+         }
+        };
     }
 
     @Override
-    protected Plugin castToSpecifiedPlugin(DataSnapshot pluginSpecificData, DataSnapshot pluginDataList) {
-        return null;
+    protected PluginPoll castToSpecifiedPlugin(DataSnapshot pluginSpecificData, DataSnapshot pluginDataList) {
+        ArrayList<Poll> polls = new ArrayList<>();
+        PluginPoll tempPluginPoll = pluginSpecificData.getValue(PluginPoll.class);
+
+        if(pluginSpecificData != null) {
+            for( DataSnapshot pollDs : pluginSpecificData.getChildren()){
+                Poll poll = pollDs.getValue(Poll.class);
+                polls.add(poll);
+
+            }
+        }
+        return new PluginPoll(tempPluginPoll.getId(), tempPluginPoll.getBeschreibung(), tempPluginPoll.getTyp(), tempPluginPoll.getChatRef(), polls);
     }
 
     @Override
-    protected void setPluginLayout() {
-
-    }
+    protected void setPluginLayout() { this.layout = R.layout.fragment_poll_plugin; }
 }
